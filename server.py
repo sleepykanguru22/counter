7c68331581a50f4afffe043101a697a9a78cd1f9
sleepykanguru22 <70289696+sleepykanguru22@users.noreply.github.com>
2022-05-06 19:51:54 -0700

    Initial commit

---
+++ server.py
@@ -0,0 +1,12 @@
+
+from flask import Flask, render_template, request
+app = Flask (__name__)
+@app.route('/')          # The "@" decorator associates this route with the function immediately following
+def hello_world():
+    return render_template('index.html')  # Return the string 'Hello World!' as a response
+
+
+    
+if __name__=="__main__":   # Ensure this file is being run directly and not from a different module    
+    app.run(debug=True)    # Run the app in debug mode.
+
